799355abc7482093ef41dbb1ea87dcaf0a335d36
mawa00006 <mattes.warning@gmx.de>
2025-05-06 14:03:50 +0200

    Initial Commit

---
+++ DAV Unterhaltsrechner/DAV_UnterhaltsrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAV_UnterhaltsrechnerApp.swift
+//  DAV Unterhaltsrechner
+//
+//  Created by Mattes Warning on 06.05.25.
+//
+
+import SwiftUI
+
+@main
+struct DAV_UnterhaltsrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DAV UnterhaltsrechnerTests/DAV_UnterhaltsrechnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DAV_UnterhaltsrechnerTests.swift
+//  DAV UnterhaltsrechnerTests
+//
+//  Created by Mattes Warning on 06.05.25.
+//
+
+import Testing
+@testable import DAV_Unterhaltsrechner
+
+struct DAV_UnterhaltsrechnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
