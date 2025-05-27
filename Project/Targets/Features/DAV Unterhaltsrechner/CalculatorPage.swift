79bcf1cdfa3a6f3299e22fe44ff6f7028a84c31c
piowarning <warning@pioneo.de>
2025-05-27 11:47:55 +0200

    Add variables and connect to resultpage

--- Project/Targets/Features/DAV Unterhaltsrechner/CalculatorPage.swift
+++ Project/Targets/Features/DAV Unterhaltsrechner/CalculatorPage.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct CalculatorPage: View {
+    @State private var kind1: Double = 0
+    @State private var kind2: Double = 0
+    @State private var kind3: Double = 0
+    @State private var einkommen: Double = 0
+
+    @State private var showResult = false
+    
     var body: some View {
         VStack {
             HeaderView()
-            UnterhaltsForm()
-            BerechnungButton()
+            UnterhaltsForm(
+                kind1: $kind1,
+                kind2: $kind2,
+                kind3: $kind3,
+                einkommen: $einkommen
+            )
+            .frame(maxHeight: .infinity)
+            BerechnungButton(action: {
+                showResult = true
+            })
             FooterView()
         }
+        .edgesIgnoringSafeArea(.all)
+        .frame(alignment: .leading)
         .padding()
+        .sheet(isPresented: $showResult) {
+            ResultPage(kind1: Int(kind1), kind2: Int(kind2), kind3: Int(kind3), einkommen: Int(einkommen))
+        }
     }
 }
